e0d5341351bfd4ae28bc1b2e289a2e40ea63d25a
m-triple-m <triplem656@gmail.com>
2023-06-04 11:18:50 +0530

    ..

--- frontend/src/components/main/Pro_list.jsx
+++ frontend/src/components/main/Pro_list.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import Accordion from '@mui/material/Accordion';
@@ -8,37 +8,36 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Button } from '@mui/material';
 
-
 const Pro_list = () => {
-
-  const [profile, setProfile] = useState([])
+  const [profile, setProfile] = useState([]);
 
   // const [imageData, setImageData] = useState([])
 
   const [masterList, setMasterList] = useState([]);
 
+  const [categories, setCategories] = useState([]);
 
   const fetchProfiles = async () => {
     const res = await fetch('http://localhost:5000/company/getall');
-    console.log(res.status)
+    console.log(res.status);
 
     const data = await res.json();
     console.log(data);
-    setProfile(data)
+    setProfile(data);
     setMasterList(data);
-  }
+    setCategories([...new Set(data.map((item) => item.type))]);
+  };
 
   const filterCompanies = async (category, value) => {
-
     const res = await fetch('http://localhost:5000/company/getall');
-    console.log(res.status)
+    console.log(res.status);
 
     const data = await res.json();
     console.log(data);
-    let filtered = data.filter(data => (data[category].toLowerCase().includes(value.toLowerCase())))
+    let filtered = data.filter((data) => data[category].toLowerCase().includes(value.toLowerCase()));
     console.log(filtered);
-    setProfile(filtered)
-  }
+    setProfile(filtered);
+  };
 
   //   const fetchImages = async () => {
   //     const res = await fetch('http://localhost:5000/image/getall');
@@ -51,7 +50,7 @@ const Pro_list = () => {
 
   useEffect(() => {
     fetchProfiles();
-  }, [])
+  }, []);
 
   //   useEffect(() => {
   //     fetchImages();
@@ -74,84 +73,87 @@ const Pro_list = () => {
   // }
 
   const filterByCompanyName = async (category, value) => {
-      console.log(value);
-      let filtered = masterList.filter(data => (data[category].toLowerCase().includes(value.toLowerCase())))
-      console.log(filtered);
-      setProfile(filtered);
-  }
-
+    console.log(value);
+    let filtered = masterList.filter((data) => data[category].toLowerCase().includes(value.toLowerCase()));
+    console.log(filtered);
+    setProfile(filtered);
+  };
 
   return (
-    <div className='container'>
-
-      <header className='list-header'>
-        <h1 className='text-center mt-3 display-4 text-center fw-bold'>Browse Company Profiles</h1>
-        <input className='form-control' onChange={e => filterByCompanyName('name', e.target.value)} />
+    <div className="container">
+      <header className="list-header">
+        <h1 className="text-center mt-3 display-4 text-center fw-bold">Browse Company Profiles</h1>
+        <input className="form-control" onChange={(e) => filterByCompanyName('name', e.target.value)} />
       </header>
 
-      <button className='btn btn-secondary mx-2 mt-3' onClick={() => { filterCompanies('type', 'sales') }}>
-        Sales
-      </button>
-
-      <button className='btn btn-secondary mx-2 mt-3' onClick={() => { filterCompanies('type', 'product based') }}>
-        Product Based
-      </button>
-
-      <button className='btn btn-secondary mx-2 mt-3' onClick={() => { filterCompanies('type', 'service based') }}>
-        Service Based
-      </button>
-
-      <button className='btn btn-secondary mx-2 mt-3' onClick={() => { filterCompanies('type', 'manufacturing') }}>
-        Manufacturing
-      </button>
-
-      <button className='btn btn-secondary mx-2 mt-3' onClick={fetchProfiles}>
-        View All
-      </button>
-
-      {/* <h1 className='text-center mt-3'>ALL PROFILES HERE</h1> */}
-
+      {categories.map((cat) => (
+        <button
+          className="btn btn-secondary mx-2 mt-3"
+          onClick={() => {
+            filterCompanies('type', cat);
+          }}
+        >
+          {cat}
+        </button>
+      ))}
+        <button
+          className="btn btn-secondary mx-2 mt-3"
+          onClick={() => {
+            filterCompanies('type', '');
+          }}
+        >
+          View All
+        </button>
 
       <div className="row">
         {profile.map((profiles) => (
-
           <div className="col-md-6">
             <div className="card mt-3 border border-success">
               <div className="card-body">
-                <h5 className="card-title"><b>Company Name - </b>{profiles.name}</h5>
+                <h5 className="card-title">
+                  <b>Company Name - </b>
+                  {profiles.name}
+                </h5>
                 <div>
                   <p>
-                    <b>Company Type - </b>{profiles.type}
+                    <b>Company Type - </b>
+                    {profiles.type}
                   </p>
                 </div>
                 <div>
                   <p>
-                    <b>Departments - </b>{profiles.departments}
+                    <b>Departments - </b>
+                    {profiles.departments}
                   </p>
                 </div>
                 <div>
                   <p>
-                    <b>Description - </b>{profiles.working}
+                    <b>Description - </b>
+                    {profiles.working}
                   </p>
                 </div>
                 <div>
                   <p>
-                    <b>Email - </b>{profiles.email}
+                    <b>Email - </b>
+                    {profiles.email}
                   </p>
                 </div>
                 <div>
                   <p>
-                    <b>Contact - </b>{profiles.contact}
+                    <b>Contact - </b>
+                    {profiles.contact}
                   </p>
                 </div>
                 <div>
                   <p>
-                    <b>Address - </b>{profiles.address}
+                    <b>Address - </b>
+                    {profiles.address}
                   </p>
                 </div>
 
-
-                <NavLink className='btn btn-primary' to={"/main/companydetails/"+profiles._id} >View More Details</NavLink>
+                <NavLink className="btn btn-primary" to={'/main/companydetails/' + profiles._id}>
+                  View More Details
+                </NavLink>
 
                 {/* <button type="button" className="btn btn-danger" onClick={() => (deleteProfile(profiles._id))}>
               Delete Profile
@@ -159,8 +161,6 @@ const Pro_list = () => {
               </div>
             </div>
           </div>
-
-
         ))}
       </div>
       {/* {imageData.map((img) => (
@@ -179,16 +179,11 @@ const Pro_list = () => {
 
       ))} */}
 
-
-
-
       {/* <div className='my-5'>
         <Link to='/homepage' className='mt-3'><b>Back</b></Link>
       </div> */}
-
     </div>
-  )
-}
+  );
+};
 
-export default Pro_list
-
+export default Pro_list;
